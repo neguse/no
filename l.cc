06295f3084842223bb22e57dac63e789547fb138
neguse <negcee@gmail.com>
2016-05-07 17:57:09 +0900

    hello

--- l.cc
+++ l.cc
@@ -38,14 +38,6 @@ public:
   bool ok() const { return fp_ != nullptr; }
 };
 
-struct Node {
-  typedef std::shared_ptr<Node> Ref;
-  char c;
-  Ref a;
-  Ref d;
-  Position pos;
-};
-
 enum class HierarchicalType {
   Leaf,
   Parent,
@@ -74,6 +66,14 @@ enum class SyntaxType {
   Variable,
 };
 
+struct Node {
+  typedef std::shared_ptr<Node> Ref;
+  char c;
+  Ref a;
+  Ref d;
+  Position pos;
+  SyntaxType st;
+};
 typedef std::vector<SyntaxType> AcceptableSyntax;
 struct Syntax {
   HierarchicalType ht;
@@ -287,6 +287,7 @@ public:
 
     auto syntax = syntax_[c];
     auto n = std::make_shared<Node>(Node{c});
+    n->st = syntax.st;
     n->pos = input.pos();
     if (syntax.ht == HierarchicalType::Parent) {
       n->a = this->ParseNode(input, level + 1);
@@ -346,6 +347,316 @@ public:
   }
 };
 
+int hex2i(char c) {
+  if ('0' <= c && c <= '9') {
+    return c - '0';
+  } else {
+    return (c - 'a') + 0xa;
+  }
+}
+
+class LangInterpreter {
+private:
+  int argc_;
+  char **argv_;
+
+public:
+  enum class ValueKind {
+    Integer,
+    List,
+    Closure,
+  };
+
+  struct Value;
+  struct Frame {
+    typedef std::shared_ptr<Frame> Ref;
+    Ref parent;
+    std::map<char, std::shared_ptr<Value>> m;
+
+    void Set(char c, std::shared_ptr<Value> v) {
+      auto it = m.find(c);
+      if (it != m.end()) {
+        it->second = v;
+      } else {
+        if (parent == nullptr) {
+          m[c] = v;
+        } else {
+          parent->Set(c, v);
+        }
+      }
+    }
+    std::shared_ptr<Value> Get(char c) const {
+      auto it = m.find(c);
+      if (it != m.end()) {
+        return it->second;
+      } else if (parent != nullptr) {
+        return parent->Get(c);
+      } else {
+        return nullptr;
+      }
+    }
+  };
+  struct Env {
+    Frame::Ref root;
+  };
+  struct Cons {
+    typedef std::shared_ptr<Cons> Ref;
+    std::shared_ptr<Value> car;
+    std::shared_ptr<Value> cdr;
+  };
+  enum class ClosureType {
+    User = 0,
+    I,
+    S,
+    A,
+    D,
+  };
+  struct Closure {
+    typedef std::shared_ptr<Closure> Ref;
+    Frame::Ref e;
+    Node::Ref p;
+    ClosureType t;
+  };
+  struct Value {
+    typedef std::shared_ptr<Value> Ref;
+    ValueKind kind;
+    struct { // unionにするとコンパイルエラーになる…
+      int i;
+      Cons::Ref l;
+      Closure::Ref c;
+    } v;
+    int GetInteger() const {
+      assert(kind == ValueKind::Integer);
+      return v.i;
+    }
+    void SetInteger(int i) {
+      kind = ValueKind::Integer;
+      v.i = i;
+    }
+    Cons::Ref GetList() const {
+      assert(kind == ValueKind::List);
+      return v.l;
+    }
+    void SetList(Cons::Ref l) {
+      kind = ValueKind::List;
+      v.l = l;
+    }
+    Closure::Ref GetClosure() {
+      assert(kind == ValueKind::Closure);
+      return v.c;
+    }
+    void SetClosure(Closure::Ref c) {
+      kind = ValueKind::Closure;
+      v.c = c;
+    }
+    bool True() {
+      switch (kind) {
+      case ValueKind::Integer:
+        return v.i != 0;
+      case ValueKind::List:
+        return v.c != nullptr;
+      case ValueKind::Closure:
+        return v.l != nullptr;
+      }
+      assert(0);
+      return false;
+    }
+    void Print(int level = 0) const {
+      for (int i = 0; i < level; i++) {
+        printf(" ");
+      }
+      switch (kind) {
+      case ValueKind::Integer: {
+        printf("<Integer>%d\n", GetInteger());
+      } break;
+      case ValueKind::List: {
+        if (v.l == nullptr) {
+          printf("<List>NULL");
+        } else {
+          printf("<List>\n");
+          printf("car:\n");
+          GetList()->car->Print(level + 1);
+          printf("cdr:\n");
+          GetList()->cdr->Print(level + 1);
+        }
+        printf("\n");
+      } break;
+      case ValueKind::Closure:
+        printf("<Closure>\n");
+        break;
+      }
+    }
+  };
+  LangInterpreter(int argc, char **argv) : argc_(argc), argv_(argv) {}
+  Value::Ref ReadI() {
+    Value::Ref v = std::make_shared<Value>();
+    return v;
+  }
+  Value::Ref ReadS() {
+    Value::Ref v = std::make_shared<Value>();
+    return v;
+  }
+  void PrintAsString(Value::Ref v) {
+    if (v->kind == ValueKind::Integer) {
+      printf("%d\n", v->GetInteger());
+    } else if (v->kind == ValueKind::List) {
+      for (auto row = v; row->GetList() != nullptr; row = row->GetList()->cdr) {
+        for (auto col = row->GetList()->car; col->GetList() != nullptr;
+             col = col->GetList()->cdr) {
+          printf("%c", col->GetList()->car->GetInteger());
+        }
+        printf("\n");
+      }
+    }
+  }
+  void Exec(Node::Ref n) {
+    Frame::Ref e = std::make_shared<Frame>();
+    {
+      Value::Ref v = std::make_shared<Value>();
+      v->SetClosure(
+          std::make_shared<Closure>(Closure{nullptr, nullptr, ClosureType::I}));
+      e->Set('I', v);
+    }
+    {
+      Value::Ref v = std::make_shared<Value>();
+      v->SetClosure(
+          std::make_shared<Closure>(Closure{nullptr, nullptr, ClosureType::S}));
+      e->Set('S', v);
+    }
+    {
+      Value::Ref v = std::make_shared<Value>();
+      v->SetClosure(
+          std::make_shared<Closure>(Closure{nullptr, nullptr, ClosureType::A}));
+      e->Set('A', v);
+    }
+    {
+      Value::Ref v = std::make_shared<Value>();
+      v->SetClosure(
+          std::make_shared<Closure>(Closure{nullptr, nullptr, ClosureType::D}));
+      e->Set('D', v);
+    }
+    {
+      Value::Ref v = std::make_shared<Value>();
+      v->SetList(nullptr);
+      e->Set('N', v);
+    }
+    auto v = Eval(n, e);
+    v->Print();
+    PrintAsString(v);
+  }
+  Value::Ref Eval(Node::Ref n, Frame::Ref e) {
+    switch (n->st) {
+    case SyntaxType::Program:
+    case SyntaxType::ProgramCont: {
+      auto v = Eval(n->a, e);
+      if (n->d->st == SyntaxType::ProgramEnd) {
+        return v;
+      } else {
+        return Eval(n->d, e);
+      }
+    } break;
+    case SyntaxType::AssignExp: {
+      auto k = n->a->c;
+      auto v = Eval(n->a->d, e);
+      e->Set(k, v);
+    } break;
+    case SyntaxType::CallExp: {
+      auto c = Eval(n->a, e)->GetClosure();
+      Frame::Ref f = std::make_shared<Frame>();
+      f->parent = c->e;
+
+      if (c->t == ClosureType::User) {
+
+        auto larg = c->p->a;
+        auto carg = n->d;
+        for (;;) {
+          if (larg->st == SyntaxType::LambdaArgListEnd &&
+              carg->st == SyntaxType::CallArgListEnd) {
+            break;
+          }
+          assert(larg->st != SyntaxType::LambdaArgListEnd);
+          assert(carg->st != SyntaxType::CallArgListEnd);
+          auto arg = Eval(carg->a, e);
+          f->Set(larg->a->c, arg);
+          larg = larg->d;
+          carg = carg->d;
+        }
+        return Eval(c->p->d, f);
+      } else {
+        switch (c->t) {
+        case ClosureType::I:
+          return ReadI();
+        case ClosureType::S:
+          return ReadS();
+        case ClosureType::A: {
+          auto l = Eval(n->d->a, e);
+          return l->GetList()->car;
+        }
+        case ClosureType::D: {
+          auto l = Eval(n->d->a, e);
+          return l->GetList()->cdr;
+        }
+        }
+        assert(0);
+        return nullptr;
+      }
+    } break;
+    case SyntaxType::LambdaExp: {
+      Value::Ref v = std::make_shared<Value>();
+      v->SetClosure(std::make_shared<Closure>(Closure{e, n}));
+    } break;
+    case SyntaxType::OpExp: {
+      auto a1 = Eval(n->a, e);
+      auto a2 = Eval(n->d, e);
+      Value::Ref v = std::make_shared<Value>();
+      switch (n->c) {
+      case '+':
+        v->SetInteger(a1->GetInteger() + a2->GetInteger());
+        return v;
+      case '-':
+        v->SetInteger(a1->GetInteger() - a2->GetInteger());
+        return v;
+      case '<':
+        v->SetInteger(a1->GetInteger() < a2->GetInteger() ? 1 : 0);
+        return v;
+      case '>':
+        v->SetInteger(a1->GetInteger() > a2->GetInteger() ? 1 : 0);
+        return v;
+      case ':': {
+        auto c = std::make_shared<Cons>(Cons{a1, a2});
+        v->SetList(c);
+        return v;
+      }
+      }
+      assert(0);
+      return nullptr;
+    } break;
+    case SyntaxType::IfExp: {
+      auto cond = Eval(n->a, e);
+      if (cond->True()) {
+        Eval(n->d->a, e);
+      } else {
+        Eval(n->d->d, e);
+      }
+    } break;
+    case SyntaxType::Hex2: {
+      Value::Ref v = std::make_shared<Value>();
+      int i = (hex2i(n->a->c) << 4) | hex2i(n->d->c);
+      v->SetInteger(i);
+      return v;
+    } break;
+    case SyntaxType::Hex: {
+      Value::Ref v = std::make_shared<Value>();
+      v->SetInteger(hex2i(n->c));
+      return v;
+    } break;
+    case SyntaxType::Variable: {
+      return e->Get(n->c);
+    } break;
+    }
+  }
+};
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     printf("Usage: ./l <program>\n");
@@ -367,6 +678,9 @@ int main(int argc, char **argv) {
   if (!l.Validate(n)) {
     return 3;
   }
+
+  auto i = LangInterpreter(argc - 2, argv + 2);
+  i.Exec(n);
 
   return 0;
 }
